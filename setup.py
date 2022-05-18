772d68ed0b8eadedce953fb633ba0e24724049d7
Zhang Yanbo <Zhang.Yanbo@asu.edu>
2022-05-18 12:06:37 -0700

    update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="CA",
-    version="1.0.0",
+    version="1.1.0",
     author="Yanbo Zhang",
     author_email="Zhang.Yanbo@asu.edu",
     description="A package for implementing cellular automata",
